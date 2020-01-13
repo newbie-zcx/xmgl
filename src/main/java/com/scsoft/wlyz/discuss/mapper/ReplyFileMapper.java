ce85430c581341ee3bbf188b0bb15d6156b05c61
ontheroad_2012@163.com <2367208893@qq.com>
2020-01-13 11:05:44 +0800

    【新增回复信息】

---
+++ src/main/java/com/scsoft/wlyz/discuss/mapper/ReplyFileMapper.java
@@ -0,0 +1,16 @@
+package com.scsoft.wlyz.discuss.mapper;
+
+import com.scsoft.wlyz.discuss.entity.ReplyFile;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 回复附件关系表 Mapper 接口
+ * </p>
+ *
+ * @author wangchao
+ * @since 2020-01-13
+ */
+public interface ReplyFileMapper extends BaseMapper<ReplyFile> {
+
+}
