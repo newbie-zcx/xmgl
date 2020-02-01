0f97fe4bf6a66f51099514516f4a69550a70bfdb
zhaopengfei <fdxqn1314@163.com>
2020-02-01 08:34:42 +0800

    添加疫情

--- src/main/java/com/scsoft/xgsb/system/controller/MainController.java
+++ src/main/java/com/scsoft/xgsb/system/controller/MainController.java
@@ -57,11 +57,16 @@ public class MainController extends BaseController implements ErrorController {
      */
     @RequestMapping({"/", "/index"})
     public String index(Model model, HttpServletRequest request,HttpSession session) {
-        List<Authority> authorities = authoritiesService.listByUserId(SystemCommonHandler.getLoginUserId());
-        List<Map<String, Object>> menuTree = getMenuTree(authorities, -1);
-        model.addAttribute("menus", menuTree);
-        model.addAttribute("loginUser", SystemCommonHandler.getLoginUser());
-        request.getSession().setAttribute("loginUser", SystemCommonHandler.getLoginUser());
+        if(!JudgeIsMoblie(request)){
+            List<Authority> authorities = authoritiesService.listByUserId(SystemCommonHandler.getLoginUserId());
+            List<Map<String, Object>> menuTree = getMenuTree(authorities, -1);
+            model.addAttribute("menus", menuTree);
+            model.addAttribute("loginUser", SystemCommonHandler.getLoginUser());
+            request.getSession().setAttribute("loginUser", SystemCommonHandler.getLoginUser());
+        }else {
+            return "redirect:/yqsb/detail/detailadd";
+        }
+
         return "index";
     }
 
@@ -228,5 +233,34 @@ public class MainController extends BaseController implements ErrorController {
         return "tpl/theme";
     }
 
+
+
+    public boolean JudgeIsMoblie(HttpServletRequest request) {
+        boolean isMoblie = false;
+        String[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
+                "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod",
+                "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
+                "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
+                "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem",
+                "wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos",
+                "pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320",
+                "240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac",
+                "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs",
+                "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi",
+                "mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port",
+                "prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
+                "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v",
+                "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-",
+                "Googlebot-Mobile" };
+        if (request.getHeader("User-Agent") != null) {
+            for (String mobileAgent : mobileAgents) {
+                if (request.getHeader("User-Agent").toLowerCase().indexOf(mobileAgent) >= 0) {
+                    isMoblie = true;
+                    break;
+                }
+            }
+        }
+        return isMoblie;
+    }
 
 }
